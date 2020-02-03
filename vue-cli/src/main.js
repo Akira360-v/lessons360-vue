7cb43b5a528960c9f9caa7e61761a3cbf30f424d
Akira360 <akira360.v@gmail.com>
2020-02-03 15:38:55 +0300

    rt06+++

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -5,7 +5,7 @@ import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 
 import App from './App.vue'
-import router from './router'
+import router from './router.js'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
 import currencyFilter from '@/filters/currency.filter'
