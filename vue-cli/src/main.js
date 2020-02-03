fb2236d2e9fee61132180fe643153ffdf2729b0a
Akira360 <akira360.v@gmail.com>
2020-02-03 16:49:54 +0300

    rt09+++

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -3,6 +3,7 @@
 /* -------------------------------------------------------------------------- */
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
 
 import App from './App.vue'
 import router from './router.js'
@@ -28,6 +29,7 @@ Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
 
 
 firebase.initializeApp({
