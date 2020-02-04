05a2124e320a0fed4ae1fd4703e4e6ef092570ff
Akira360 <akira360.v@gmail.com>
2020-02-04 11:03:54 +0300

    rt12+++

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <div id="app">
-    <div class="container header_container">
-      <div class="header__menu">
-        <div id="nav">
-          <router-link to="/">Home</router-link>
-          <router-link to="/login">Login</router-link>
-          <router-link to="/categories">Categories</router-link>
-          <router-link to="/history">history</router-link>
-        </div>
-        <div class="header__logo">
-          <img src="" alt="logo">
-        </div>
-      </div>
-    </div>
     <component :is='layout'>
       <router-view />
     </component>
